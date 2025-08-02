641fc7af0c35332ac40af058f009314965ae3f61
emrekocaklar579 <emrekocaklar579@gmail.com>
2025-08-03 01:18:43 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-charts'],
+  css: ['~/assets/css/tailwind.css'],
 
   imports: {
     autoImport: true,
